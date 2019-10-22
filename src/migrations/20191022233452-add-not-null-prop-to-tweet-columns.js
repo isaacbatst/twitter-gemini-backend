1f2e6c3451740bb107d25593b0c9e5a7a5d2e2a7
Isaac Batista <isaacbatst@gmail.com>
2019-10-22 20:50:06 -0300

    migration: adds not_null prop to tweet columns

---
+++ src/migrations/20191022233452-add-not-null-prop-to-tweet-columns.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface
+      .changeColumn("Tweets", "message", {
+        type: Sequelize.STRING,
+        allowNull: false
+      })
+      .then(() =>
+        queryInterface.changeColumn("Tweets", "favorite", {
+          type: Sequelize.BOOLEAN,
+          allowNull: false
+        })
+      );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface
+      .changeColumn("Tweets", "message", {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      })
+      .then(() =>
+        queryInterface.changeColumn("Tweets", "favorite", {
+          type: Sequelize.BOOLEAN,
+          allowNull: true
+        })
+      );
+  }
+};
